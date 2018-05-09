01eb3b1b961a92b3ead3bebddb0df420ccf5ea1e
Egor Litvinchuk <tujhhjut@mail.ru>
2018-05-10 01:45:41 +0300

    Bug fixes

--- src/main/java/com/example/service/UserServiceImpl.java
+++ src/main/java/com/example/service/UserServiceImpl.java
@@ -4,6 +4,8 @@ import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
 
+import javax.transaction.Transactional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -24,35 +26,42 @@ public class UserServiceImpl implements UserService{
     private BCryptPasswordEncoder bCryptPasswordEncoder;
 	
     @Override
+    @Transactional
 	public User findUserByEmail(String email) {
 		return userRepository.findByEmail(email);
 	}
 
     @Override
+    @Transactional
 	public User findUserByEmail(String email, Long id) {
 		return userRepository.findByEmail(email, id);
 	}
 	
 	@Override
+	@Transactional
 	public User findUserById(Long id) {
 		return userRepository.findOne(id);
 	}
 
 	@Override
+	@Transactional
 	public User findUserByPassportId(String passportid) {
 		return userRepository.findByPassportId(passportid);
 	}
 	
 	@Override
+	@Transactional
 	public User findUserByPassportId(String passportid, Long id) {
 		return userRepository.findByPassportId(passportid, id);
 	}
 	
 	@Override
+	@Transactional
 	public List<User> findAll() {
 		return userRepository.findAll();
 	}
 	@Override
+	@Transactional
 	public void saveUser(User user) {
 		user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
         user.setActive(2);
@@ -61,6 +70,7 @@ public class UserServiceImpl implements UserService{
 		userRepository.save(user);
 	}
 	@Override
+	@Transactional
 	public void deleteUser(User user) {
 		userRepository.delete(user);
 	}
